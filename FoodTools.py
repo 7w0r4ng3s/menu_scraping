8674a49c3b1063dc1af3c8d94763b19c07c7ada1
7w0r4ng3s <chr1s_liu@icloud.com>
2018-09-25 22:38:14 -0700

    add report

--- FoodTools.py
+++ FoodTools.py
@@ -6,7 +6,7 @@ foods = list(df['food'].unique()) # a list of unique foods
 
 # return the number of times a food appears and the number of time you like it
 def search():
-    food_name = input('Food name: ') # works whether you type it uppercase or lowercase
+    food_name = input('Search food: ') # works whether you type it uppercase or lowercase
     if food_name in foods:
         new_df = df.loc[df['food'] == food_name]
         total = new_df['food'].count() # total times the food appears
@@ -18,22 +18,18 @@ def search():
         
     elif food_name not in foods:
         tmp = [] # a list to store possible foods
-        """
-        if the user type a part of a food name
-        it will return the possible food names
-        """
+        """if the user type a part of a food name
+        it will return the possible food names"""
         
         [tmp.append(item) for item in foods if food_name.lower() in item.lower()] # possible food list
         l = [i+1 for i in list(range(len(tmp)))] # index list
 
-        # no search results
         if len(tmp) == 0:
             print(f'No result related to {food_name}. Please try again.')
             return None
 
         print('Do you mean:')
 
-        # show search results (related)
         food_dict = dict(zip(l, tmp))
         for i in food_dict.keys():
             print(i, food_dict[i])
@@ -49,5 +45,24 @@ def search():
         print('-' * 80)
         print(f'Out of {total} time(s), you like [{food_dict[choice]}] {like} time(s).')
         print('-' * 80)
-    
-    return score
+return score
+
+
+def report(food_name):
+    new_df = df.loc[df['food'] == food_name]
+    total = new_df['food'].count() # total times the food appears
+    like = new_df.iloc[:, 1].sum(0) # number of times you like it
+    food_score = like / total
+    print('[{}] score: '.format(food_name), food_score)
+    # a simple food preference evaluation to a specific food
+    if food_score > 0.5:
+        print('You like [{food_name}] over half of the time.')
+    elif food_score < 0.5:
+        print('You like [{food_name}] below half of the time.')
+    else:
+        print('50-50')
+
+
+# an algorithm which calculates a 'food value'
+# food value - a value evaluating the likelihood you will like the menu of one day
+# based on the food preference database
